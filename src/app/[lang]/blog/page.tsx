91d511e3e405adda2f82527ba8374f1d010e4df4
laurinbrechter <brechterlaurin@gmail.com>
2024-09-19 20:49:37 +0200

    add own blogs and medium blogs separately

--- src/app/[lang]/blog/page.tsx
+++ src/app/[lang]/blog/page.tsx
@@ -3,10 +3,13 @@ import fs from "fs";
 
 const Page = () => {
   
-  const blog_json = JSON.parse(fs.readFileSync(process.cwd() + '/src/app/[lang]/blog/blogs.json', 'utf8'));
-  
+  const medium_blogs = JSON.parse(fs.readFileSync(process.cwd() + '/src/app/[lang]/blog/medium_blogs.json', 'utf8'));
+  const own_blogs = JSON.parse(fs.readFileSync(process.cwd() + '/src/app/[lang]/blog/own_blogs.json', 'utf8'));
+
+  const blogs = medium_blogs.concat(own_blogs);
+
   return (
-    <Blog blog_data={blog_json} />
+    <Blog blog_data={blogs} />
   )
 }
 
